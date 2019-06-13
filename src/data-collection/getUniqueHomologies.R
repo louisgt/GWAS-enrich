7600134bc7b03980394357e3bc5fa78aae2d4e62
Louis Gauthier <louis.gauthier@umontreal.ca>
2019-06-13 14:40:12 -0400

    find unique homologies with biomaRt, add code for replicability

---
+++ src/data-collection/getUniqueHomologies.R
@@ -0,0 +1,19 @@
+library(biomaRt)
+
+mart.hs <- useMart("ensembl", "hsapiens_gene_ensembl")
+mart.sc <- useMart("ensembl", "scerevisiae_gene_ensembl")
+mart.snp = useMart(biomart="ENSEMBL_MART_SNP", host="grch37.ensembl.org", path="/biomart/martservice",
+                   dataset="hsapiens_snp")
+
+human.homolog = tryCatch(getLDS(attributes=c("chromosome_name","ensembl_gene_id"), filters="ensembl_gene_id",
+values=hdPCA_list$ID, mart=mart.sc, attributesL=c("chromosome_name","hgnc_symbol","ensembl_gene_id"), martL=mart.hs), error=function(cond) { return(c()) } )
+
+orf.gwas = as.data.frame(unique(human.homolog$Gene.stable.ID.1))
+homologies = human.homolog[,c(2,4,5)]
+colnames(homologies)=c("yeast_id","human_name","human_id")
+unique_homologies = homologies[!duplicated(t(apply(homologies,1,sort))),]
+df = merge(unique_homologies,hdPCA_list,by.x = "yeast_id",by.y = "X3")
+df$X1=NULL
+df2 = df[,c(4,1,2,3)]
+colnames(df2)[1]="yeast_name"
+write_csv(df2,path = "Documents/PROJECTS/GWAS-enrich/references/unique_homologies.csv",col_names = TRUE,quote_escape = FALSE)
